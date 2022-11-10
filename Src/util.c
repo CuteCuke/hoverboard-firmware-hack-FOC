fce3fc1bf66f6a7ceed2f52fec5ff03b1dcf9fb7
onionYJK <1548240981@qq.com>
2022-11-10 09:48:13 +0800

    cmd50

--- Src/util.c
+++ Src/util.c
@@ -739,7 +739,7 @@ void standstillHold(void)
 #if defined(STANDSTILL_HOLD_ENABLE) && (CTRL_TYP_SEL == FOC_CTRL) && (CTRL_MOD_REQ != SPD_MODE)
   if (!rtP_Left.b_cruiseCtrlEna)
   {                                                                               // If Stanstill in NOT Active -> try Activation
-    if (((abs(input2[inIdx].cmd )<=10&&abs(input1[inIdx].cmd)<=10) && speedAvgAbs < 60) // Check if Brake is pressed AND measured speed is small //input1[inIdx].cmd > 50 || 
+    if (((abs(input2[inIdx].cmd )<=50&&abs(input1[inIdx].cmd)<=50) && speedAvgAbs < 60) // Check if Brake is pressed AND measured speed is small //input1[inIdx].cmd > 50 || 
        )// || (input2[inIdx].cmd < 20 && speedAvgAbs < 5)
     { // OR Throttle is small AND measured speed is very small
       rtP_Left.n_cruiseMotTgt = 0;
@@ -753,7 +753,7 @@ void standstillHold(void)
   {
     
       //(input1[inIdx].cmd < 20 && input2[inIdx].cmd > 50)                                                  // If Stanstill is Active -> try Deactivation
-      if ((abs(input2[inIdx].cmd )>10||abs(input1[inIdx].cmd) > 10)&&!cruiseCtrlAcv)
+      if ((abs(input2[inIdx].cmd )>50||abs(input1[inIdx].cmd) > 50)&&!cruiseCtrlAcv)
       { // Check if Brake is released AND Throttle is pressed AND no Cruise Control
         rtP_Left.b_cruiseCtrlEna = 0;
         rtP_Right.b_cruiseCtrlEna = 0;
